51508c6e89cba133daa7391ec7602e795e4431be
Michael Kachi <jesmikky@gmail.com>
2022-11-05 22:41:05 +0100

    error pages added

--- app/resources/views/errors/500.blade.php
+++ app/resources/views/errors/500.blade.php
@@ -2,61 +2,40 @@
 
 @section('content')
 
-<main class="main">
-		<section class="header-page">
-			<div class="container">
-				<div class="row">
-					<div class="col-sm-3 hidden-xs">
-						<h1 class="mh-title">Home</h1>
-					</div>
-					<div class="breadcrumb-w col-sm-9">
-					
-						<ul class="breadcrumb">
-							<li>
-								<a href="index.html">Home</a>
-							</li>
-							<li>
-								<span>Errors</span>
-							</li>
-						</ul>
-					</div>
-				</div>
+<main id="content" role="main" class="checkout-page">
+	<!-- breadcrumb -->
+	<div class="bg-gray-13 bg-md-transparent">
+		<div class="container">
+			<!-- breadcrumb -->
+			<div class="my-md-3">
+				<nav aria-label="breadcrumb">
+					<ol class="breadcrumb mb-3 flex-nowrap flex-xl-wrap overflow-auto overflow-xl-visble">
+						<li class="breadcrumb-item flex-shrink-0 flex-xl-shrink-1"><a href="">Home</a></li>
+						<li class="breadcrumb-item flex-shrink-0 flex-xl-shrink-1 active" aria-current="page">Errors</li>
+					</ol>
+				</nav>
 			</div>
-		</section>
+			<!-- End breadcrumb -->
+		</div>
+	</div>
 		
-		<section class="product-info-w">
-			<div class="container">
-				<div class="row" style="padding-top:100px">
-				
-				
-				</div>
 
-				<div class="row">
-					<div class="tab-content clearfix">
-					    <div role="tabpanel" class="tab-pane active" id="features">
-					    	<div class="product-image v-middle">
-						    	<div class="col-sm-12 col-xs-12">
-						    		<img src="{{asset('/error.png')}}" alt="ideal 1">
-						    	</div>
-						    </div>
-						    <div class="product-shortdescript v-middle">
-								<div class="col-sm-12 col-xs-12">
-									<div class="v-middle">
-										<h3>An error Occured</h3>
-							    		<ul>
-							    			<li><i class="fa fa-check"></i> Seems something went wrong</li>
-							    			<a href="{{route('index')}}" class="btn btn-primary"> Return Home</a>
-										
-							    		</ul>
-							    	</div>
-								</div>
-							</div>
-					    </div>
-				
-					   
-					</div>
-				</div>
+
+		
+            <div class="container">
+                    <div class="row">
+						<div style="" class="mb-7 mb-lg-0 p-3">
+                            <div >
+                                <div class="bg-gray-1 rounded-lg p-2">
+                                        <div class=" mb-5">
+                                            <p class=" mb-0 p-3 "><i class="fa fa-check"></i> Something went wrong, refresh page</p>
+											<a href="{{route('index')}}" class="btn btn-primary btn-sm p-2"> Return Home</a>
+									
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
 			</div>
-		</section>
+		</main>
 
 @endsection
